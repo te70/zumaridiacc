54e8683e02632242e476de8a4d9495ee6500c57d
te70 <tevinkinuthia@gmail.com>
2023-03-17 16:52:57 +0300

    Add icons to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,14 @@
-<x-app-layout>
+@extends('layouts.app')
+@section('content')
     <div class="m-3">
         <div class="container">
-            <div class="row ms-auto me-auto" style="">          
-              <div class="col-xs-8 col-sm-3 col-md-3">
+            <div class="row pt-2" style="margin-left: 150px;">          
+              <div class="col-xs-8 col-sm-3 col-md-3 pb-2">
                 <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);">
                   <div class="card-body">
-                    <h4 class="card-text" style="text-align: center;">{{$winesRevenue}}</h4>
-                    <p class="card-title" style="font-size: 20px; text-align: center;">Wines revenue</p>
+                    <h4 class="card-text" style="">{{$winesRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Total rooms</p>
+                    <span class="bi bi-houses" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
                   </div>
                 </div>
               </div>
@@ -14,8 +16,9 @@
               <div class="col-xs-8 col-sm-3 col-md-3">
                 <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);">
                   <div class="card-body">
-                    <h4 class="card-text" style="text-align: center;">{{$barRevenue}}</h4>
-                    <p class="card-title" style="font-size: 20px; text-align: center;">Bar revenue</p>
+                    <h4 class="card-text">{{$barRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Reservations</p>
+                    <span class="bi bi-bookmark" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
                   </div>
                 </div>
               </div>
@@ -23,8 +26,9 @@
               <div class="col-xs-8 col-sm-3 col-md-3">
                 <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
                   <div class="card-body">
-                    <h4 class="card-text" style="text-align: center;">{{$psRevenue}}</h4>
-                    <p class="card-title" style="font-size: 20px; text-align: center;">Playstation revenue</p>
+                    <h4 class="card-text">{{$psRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Staffs</p>
+                    <span class="bi bi-people" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
                   </div>
                 </div>
               </div>
@@ -32,15 +36,93 @@
               <div class="col-xs-8 col-sm-3 col-md-3">
                 <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
                   <div class="card-body">
-                    <h4 class="card-text" style="text-align: center;">{{$ibRevenue}}</h4>
-                    <p class="card-title" style="font-size: 20px; text-align: center;">Inbet revenue</p>
+                    <h4 class="card-text">{{$ibRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Complaints</p>
+                    <span class="bi bi-chat" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-xs-8 col-sm-3 col-md-3 pb-2">
+                <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
+                  <div class="card-body">
+                    <h4 class="card-text">{{$ibRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Booked rooms</p>
+                    <span class="bi bi-bookmark-check" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-xs-8 col-sm-3 col-md-3">
+                <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
+                  <div class="card-body">
+                    <h4 class="card-text">{{$ibRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Available rooms</p>
+                    <span class="bi bi-house-add" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-xs-8 col-sm-3 col-md-3">
+                <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
+                  <div class="card-body">
+                    <h4 class="card-text">{{$ibRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Items in stock</p>
+                    <span class="bi bi-archive" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-xs-8 col-sm-3 col-md-3">
+                <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
+                  <div class="card-body">
+                    <h4 class="card-text">Ksh.{{$ibRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Rooms</p>
+                    <span class="bi bi-cash" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-xs-8 col-sm-3 col-md-3">
+                <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
+                  <div class="card-body">
+                    <h4 class="card-text">Ksh.{{$ibRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Wines</p>
+                    <span class="bi bi-cash" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-xs-8 col-sm-3 col-md-3">
+                <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
+                  <div class="card-body">
+                    <h4 class="card-text">Ksh.{{$ibRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Bar</p>
+                    <span class="bi bi-cash" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-xs-8 col-sm-3 col-md-3">
+                <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
+                  <div class="card-body">
+                    <h4 class="card-text">Ksh.{{$ibRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Inbet</p>
+                    <span class="bi bi-cash" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-xs-8 col-sm-3 col-md-3">
+                <div class="card" style="border: none; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"> 
+                  <div class="card-body">
+                    <h4 class="card-text">Ksh.{{$ibRevenue}}</h4>
+                    <p class="card-title" style="font-size: 20px;">Playstation</p>
+                    <span class="bi bi-cash" style="position: absolute; top: 50%; right: 15px; transform: translateY(-50%); font-size: 32px;"></span>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="btn-group mr-2 pt-8" style="margin-left: 250px;">
-            <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
           </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"></script>
@@ -70,4 +152,4 @@
         }
       });
     </script>
-</x-app-layout>
+@endsection
